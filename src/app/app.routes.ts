5a610690b7e67dda3f209181ed2504717012693e
crayolito <josalejandroapp2017@gmail.com>
2024-10-07 20:43:58 -0400

    hoy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,12 +22,12 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/home',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
     path: '**',
-    redirectTo: '/home',
+    redirectTo: 'home',
   },
 ];
 
